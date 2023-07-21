ac194c8ffb8c4a5b4467a60c5705ba83d2fded44
Syriamme <Roridaniel01@gmail.com>
2023-07-22 00:07:33 +0300

    checking if the last digit of a random number is positive or negative

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,13 @@
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+
+results = "Last digit of {} is {}".format(number, last)
+if last > 5:
+    results += " and is greater than 5"
+elif last == 0:
+    results += " and is 0"
+else:
+    results += " and is less than 6 and not 0"
+
+print(results)
