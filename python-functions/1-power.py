4d6515423e6123d23439ed2760a9fbe0a1a85e7a
Syriamme <Roridaniel01@gmail.com>
2023-07-22 19:33:50 +0300

    a program that calculates the power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,17 +1,9 @@
-def pow(x, y):
-    """
-    Given two numbers, (x, y), return the power.
-    :param x: int
-    :param y: int
-    :return: int
-    """
-    power = 1
-    for _ in range(y):
-        power *= x
-    return power
-
-# Test cases
-assert pow(2, 3) == 8
-assert pow(5, 0) == 1
-assert pow(13, 15) == 68719476736
-assert pow(5, 8) == 390625
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result /= a
+    return result
