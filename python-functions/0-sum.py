f86c9d63d7166294d39ec6fdf41e514bd47a33fb
Syriamme <Roridaniel01@gmail.com>
2023-07-22 19:03:33 +0300

    adding numbers

---
+++ python-functions/0-sum.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    """
+    Given to numbers, (a, b), return the sum.
+    :param a: int
+    :param b: int
+    :return: int
+    example
+    >>> add(5, 8)
+    13
+    >>> add(13, 15)
+    28
+    """
+    return a + b
+if __name__ == "__main__":
+    result = add(a, b)
+    print("Result:", result)
