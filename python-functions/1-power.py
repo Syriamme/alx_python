c1745e9d7eecefdfccc3021fa8db175548be6832
Syriamme <Roridaniel01@gmail.com>
2023-07-22 19:54:44 +0300

    a program that calculates the power of a number

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,22 +1,21 @@
+"""
+Given two numbers (a, b), calculate the power and return a value
+:param a: int
+:param b: int
+:return: int
+example
+>>> power(2, 3
+8
+return (a ** b)
+"""
 def power(a, b):
-    """
-    Given two numbers (a, b), calculate the power and return a value
-    :param a: int
-    :param b: int
-    :return: int
-    example
-
-    >>> power(2, 3)
-    8
-    return (a ** b)
-    """
-    results = 1
+    result = 1
     if b >= 0:
         for i in range(b):
-            results *= a
+            result *= a
     else:
         b = -b
         for i in range(b):
-            results *= a
-            results = 1 / results
-    return results
+            result *= a
+        result = 1 / result
+    return result
