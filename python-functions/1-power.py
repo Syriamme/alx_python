f9132589702090743df9e75c3b6eed98d4918259
Syriamme <Roridaniel01@gmail.com>
2023-07-22 19:57:14 +0300

    a program that calculates the power of a number

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -9,13 +9,16 @@ example
 return (a ** b)
 """
 def pow(a, b):
-    result = 1
+    final = 1
+
     if b >= 0:
         for i in range(b):
-            result *= a
+            final *= a
+
     else:
         b = -b
         for i in range(b):
-            result *= a
-        result = 1 / result
-    return result
+            final *= a
+        final = 1 / final
+        
+    return final
