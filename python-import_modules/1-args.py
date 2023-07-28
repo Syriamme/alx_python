5307de11c192882f0fef6bc063c78b01ab6b55ca
Syriamme <Roridaniel01@gmail.com>
2023-07-28 11:16:36 +0300

    argument count

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,22 @@
 import sys
 
-def print_args_info():
-    argv = sys.argv
-    num_args = len(argv) - 1  # Subtract 1 because argv[0] is the script name itself
+def main():
 
-    if num_args > 0:
-        arg_suffix = "argument:" if num_args == 1 else "arguments:"
-        print(f"{num_args} {arg_suffix}")
-        for i in range(1, len(argv)):
-            print(f"{i}: {argv[i]}")
+    argument = sys.argv
+
+    arg_list = len(argument) - 1
+
+    if arg_list > 0:
+
+        names = "argument:" if arg_list == 1 else "arguments:"
+
+        print(f"{arg_list} {names}")
+
+        for i in range(1, len(argument)):
+
+            print(f"{i}: {argument[i]}")
     else:
-        print("0 arguments.")
+        print(".")
 
 if __name__ == "__main__":
-    print_args_info()
+    main()
