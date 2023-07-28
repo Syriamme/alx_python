d54e4f070ad06be86e119487fdd8bda79ffb100f
Syriamme <Roridaniel01@gmail.com>
2023-07-28 12:00:36 +0300

    division and exception handling

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,9 +1,13 @@
 def safe_print_division(a, b):
     result = None
+
     try:
         result = a / b
-    except Exception as e:
-        pass
+        
+    except Exception as n:
+        print ("Error: {}" .format(n))
+    
     finally:
         print("Inside result: {}".format(result))
+
     return result
