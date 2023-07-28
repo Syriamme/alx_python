9b8e595df29421ca09f162348b884fe8397c2608
Syriamme <Roridaniel01@gmail.com>
2023-07-28 10:17:50 +0300

    adding two numbers

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,9 +3,11 @@ def add(a, b):
 
 a = 1
 b = 2
+
 from add_0 import add
 
 adding = add(a, b)
+print("{} + {} = {}".format(a, b, adding))
 
 if __name__ == "_main_":
-    print("{} + {} = {}".format(a, b, adding))
+    main()
