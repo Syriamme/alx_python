c552a17aff24294f8fa61d29243e968fcebbf10a
Syriamme <Roridaniel01@gmail.com>
2023-07-30 14:37:15 +0300

    printing matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,19 +1,16 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        counter = 0
-        for item in row:
-            if counter != len(row) - 1:
-                print('{:d} '.format(item), end="")
+        for i in range(len(row)):
+            # if it's not the last element, print with ' | ' and no newline
+            if i != len(row) - 1:
+                print('{:d} | '.format(row[i]), end="")
+            # if it's the last element, just print the element and newline
             else:
-                print('{:d}'.format(item))
-            counter += 1
+                print('{:d}'.format(row[i]))
 
 def main():
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print_matrix_integer(matrix)
+
 if __name__ == "__main__":
     main()
