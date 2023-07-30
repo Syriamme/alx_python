a7228f2a1fd7da36c6a26b30882078cb1ecf39e2
Syriamme <Roridaniel01@gmail.com>
2023-07-30 14:30:23 +0300

    printing matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -7,3 +7,11 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print('{:d}'.format(item))
             counter += 1
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print(print_matrix_integer(matrix))
