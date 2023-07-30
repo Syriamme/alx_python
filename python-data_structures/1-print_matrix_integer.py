247d0ea1f93aa343a9d8dab4ef3b6ac7b00c2e3c
Syriamme <Roridaniel01@gmail.com>
2023-07-30 14:02:08 +0300

    printing a matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,17 @@
+def print_matrix_integer(matrix=[[]]):
+    
+    for rows in matrix:
+        
+        for columns in rows:
+            
+            print("{:d}".format(columns), end=" ")
+            
+        print()
+if __name__ == "__main__":
+    testing_matrix = [
+        [1, 9, 10, 14],
+        [3, 9, 16, 22],
+        [60, 27, 59, 88]
+    ]
+
+print_matrix_integer(testing_matrix)
