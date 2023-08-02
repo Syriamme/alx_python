3457bbf9c01519f01d564acbf8df3eb75a07b801
Syriamme <Roridaniel01@gmail.com>
2023-08-02 05:26:01 +0300

    best secore

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,12 +1,15 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
-    max_key = None
-    max_value = -float('inf')
+
+    # Get first key and value from the dictionary
+    max_key, max_value = list(a_dictionary.items())[0]
+
+    # Iterate over all items in the dictionary
     for key, value in a_dictionary.items():
+        # If the current value is higher than the max value, update max_key and max_value
         if value > max_value:
             max_key = key
             max_value = value
-            
+
     return max_key
