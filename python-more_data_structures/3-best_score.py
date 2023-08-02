bfc1200d36e863d5fe912fa4950d01921f98fd92
Syriamme <Roridaniel01@gmail.com>
2023-08-02 05:39:36 +0300

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,15 @@
 def best_score(a_dictionary):
+    
     if not a_dictionary:
+
         return None
 
     max_key, max_value = list(a_dictionary.items())[0]
 
     for key, value in a_dictionary.items():
+
         if value > max_value:
+
             max_key = key
             max_value = value
 
