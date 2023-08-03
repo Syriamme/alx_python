f613729ada9cd92f7f45007b9caa7bea46f31c8b
Syriamme <Roridaniel01@gmail.com>
2023-08-03 22:15:57 +0300

    size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -16,12 +16,12 @@ class Square:
         Raises:
             TypeError & valueError
         """
+        
         if not isinstance(size, int):
-            raise TypeError('size not integer')
+            raise TypeError('size must be an integer')
         
         elif size < 0:
-            raise ValueError('size not >= 0')
+            raise ValueError('size must be >= 0')
         
         else:
-
             self.__size = size
