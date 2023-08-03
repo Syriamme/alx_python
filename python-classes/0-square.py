b30aad3627b67849dc567d67bd59abc65bf8299c
Syriamme <Roridaniel01@gmail.com>
2023-08-03 21:15:37 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,17 @@
 class Square:
+    """Represents a square geometric shape.
+
+    Attributes:
+        __size (int or float): Private attribute that defines the size of the square's side. 
+
+    Note:
+        The attribute size is kept private to maintain control over its values, 
+        as it's crucial for computations related to the square (like area).
+    """
     def __init__(self, size):
+        """Initializes Square with a provided size.
+
+        Args:
+            size (int or float): The size of the square's side. 
+        """
         self.__size = size
