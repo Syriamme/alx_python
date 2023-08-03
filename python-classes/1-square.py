bd4158d2156843f657402d380a49f2f0552876f5
Syriamme <Roridaniel01@gmail.com>
2023-08-03 22:14:04 +0300

    size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,28 +1,27 @@
 """
-This module provides a Square class. The Square class allows you to create and manipulate Square objects, 
-which represent squares in a geometric context.
+Module that provides a Square class
 """
 
 class Square:
     """
-    A Square class represents a geometric square. 
-    It is characterized by a single attribute: size, which represents the length of a side of the square. 
+    Square class representing a square. 
+    Defined by a single attribute. 
     """
 
     def __init__(self, size=0):
         """
-        The constructor for the Square class.
-
-        Parameters:
-            size (int, optional): The size (length of a side) of the square. Defaults to 0.
-
+        The Square class constructor.
+        Param:
+            size: length of a side of the square set to default (0).
         Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
+            TypeError & valueError
         """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+            raise TypeError('size not integer')
+        
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError('size not >= 0')
+        
         else:
-            self.__size = size  # private attribute
+
+            self.__size = size
