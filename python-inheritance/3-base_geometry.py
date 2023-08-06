f689c5cb9ef5402541704c2711d3fc4bdb561b26
Syriamme <Roridaniel01@gmail.com>
2023-08-06 18:48:40 +0300

    basegeometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,9 @@
 """
-The module is a geometric base class which is empty which serves as a parent class 
+The module is a geometric base class which is empty
+and serves as a parent class 
 for forming geometric shapes
 """
-
-
 class BaseGeometry:
-
     """
     BaseGeometry class is currently empty.
     This can be modified with methods and attributes
