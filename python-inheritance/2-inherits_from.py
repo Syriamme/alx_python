1143723656be8d720915fef8c8847be3ce6350f7
Syriamme <Roridaniel01@gmail.com>
2023-08-06 18:15:30 +0300

    A function that returns True if the object is an instance of a class that inherited

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -5,17 +5,14 @@ that inherited from the class.
 """
 
 def inherits_from(obj, a_class):
-  """
+    """
     function to check if the object is an instance of a class inherited from the class.
-
     Params:
     - obj: The object beinf checked.
     - a_class: check inheritance against.
-
     Returns:
     True,
     if obj is an instance of a class that inherited from the class.
     Otherwise False.
     """
-  
-  return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
