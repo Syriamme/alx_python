f9c1945e9942014d911cc6a7c85a6870b89e9536
Syriamme <Roridaniel01@gmail.com>
2023-08-06 18:51:24 +0300

    empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,9 +4,9 @@ and serves as a parent class
 for forming geometric shapes
 """
 class BaseGeometry:
-    pass
     """
     BaseGeometry class is currently empty.
     This can be modified with methods and attributes
     as required for various geometric functionalities.
     """
+    pass
