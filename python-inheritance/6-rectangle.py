6e6449c991535f487fa4881a73d4474b4760eeaf
Syriamme <Roridaniel01@gmail.com>
2023-08-07 10:39:51 +0300

    a class Rectangle that inherits from BaseGeometry

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -62,9 +62,5 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-        def area(self):
-            return self.__width * self.__height
-        def __str__(self):
-            return f"[Rectangle] {self.__width}/{self.__height}"
+        
         print(issubclass(Rectangle, BaseGeometry))
