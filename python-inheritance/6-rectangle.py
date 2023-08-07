9ec05868227e683190cb2aaa857124ac64be9bbe
Syriamme <Roridaniel01@gmail.com>
2023-08-07 10:50:09 +0300

    a class Rectangle that inherits from BaseGeometry

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,42 +1,6 @@
-"""
-base_geometry module
-"""
+#!/usr/bin/python3
 
-class BaseGeometry:
-    """
-    Base class for geometric shapes
-    Attributes: No attribute
-    Methods:
-        area(self): Raises an exception if not implemented
-        integer_validator(self, name, value): Will Validate that the value
-        is positive integer
-    """
-
-    def area(self):
-        """
-        Area of the geometric shape
-
-        Raises:
-            Exception: if not implemented in the subclass
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validating positive integer
-        Args:
-            name (str): The name
-            value (int): The value
-        Raises:
-            TypeError: Value fails to be an integer
-            ValueError: Value is less than or equal to 0
-        """
-        
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
         
 class Rectangle(BaseGeometry):
     """
