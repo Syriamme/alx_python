a005038c36d2ce48612efc1e009fad1038c2ab2d
Syriamme <Roridaniel01@gmail.com>
2023-08-07 09:01:29 +0300

    a function that checks the instance of a given class

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,14 +1,13 @@
 """
-The module is a function to check if a given object is similar to an instance.
+The module - function checking if a given object is similar to an instance.
 
-It fails to consider inheritance, which means it will return False, 
-when the object is an instance of a subclass of the class.
+It fails to consider inheritance, returning False.
 
 """
 
 def is_same_class(obj, a_class):
     """
-    This function checks if the given object is an instance of the given class.
+    The function checks if an object is an instance of the given class.
     It fails to consider inheritance, and therefore 
     only returns True if the object's
     type matches the specified class exactly.
@@ -23,4 +22,11 @@ def is_same_class(obj, a_class):
     instance of the class specified
     :rtype: bool
     """
-    return type(obj) is a_class
+    object_class = type(obj)
+
+    if object_class is a_class:
+        return True
+    
+    else:
+        return False
+    
