1d16a0fec16f0d871407143f232d80ecd9f6ccf2
Syriamme <Roridaniel01@gmail.com>
2023-08-10 20:29:46 +0300

    a rectangle class inheriting from base class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,3 +28,60 @@ class Rectangle(Base):
         self.width = width
         self.y = y
         self.x = x
+    
+    @property
+    def width(self):
+        """
+        Width getter
+        """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """
+        Width setter
+        """
+        self.__width = value
+    
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        self.__height = value
+    
+    @property
+    def x(self):
+        """
+        x getter
+        """
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """
+        x setter
+        """
+        self.__x = value
+    
+    @property
+    def y(self):
+        """
+        y getter
+        """
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """
+        y setter
+        """
+        self.__y = value
+        
