55d772df3bf1dc34185bd7e4f7609feed5d60052
Syriamme <Roridaniel01@gmail.com>
2023-08-10 21:36:32 +0300

    the class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
 """
-Module to define the class Square
+The module defining the square
 """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-    class Square inherits from Rectangle
-    Rectangle is a class in the models module
+    A derived class inherited from Rectangle class
     """
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+
     def __str__(self):
-        """Modifies the inbuilt method init"""
+        """
+        Modifying the inbuilt
+        """
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y,
             self.width)
+
 
     @property
     def size(self):
@@ -26,15 +31,14 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        self.width = value
         self.height = value
+        self.width = value
 
     def update(self, *args, **kwargs):
         """
-        Function to update the attributes of square
-
-        If args: set attributes in this order: id, width, height, x, y
-        If no args given: set attributes according to kwargs
+        Updating the square attributes
+        If no args: Attributes should be set with kwargs
+        If args: attributes should be: id, width, height, x, y (in that order)
         """
         if args:
             for key, value in enumerate(args):
@@ -51,18 +55,19 @@ class Square(Rectangle):
                 self.id = kwargs["id"]
             if "size" in kwargs:
                 self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+
 
     def to_dictionary(self):
         """
-        Returns the dictionary definition of a square
+        Returning the dictionary definition
         """
         return {
             "id": self.id,
             "size": self.width,
-            "x": self.x,
             "y": self.y,
+            "x": self.x,
         }
