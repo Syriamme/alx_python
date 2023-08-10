b8abc0b7b90233698a79aacdd420eb921ac01618
Syriamme <Roridaniel01@gmail.com>
2023-08-10 20:26:05 +0300

    a rectangle class inheriting from base class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,32 +1,30 @@
+"""
+The module for rectangle class
+"""
 from models.base import Base
+
 
 class Rectangle(Base):
     """
-    Rectangle class that inherits from Base.
-    
-    Attributes:
-        width (int): Width of the rectangle.
-        height (int): Height of the rectangle.
-        x (int): x-coordinate of the rectangle's position.
-        y (int): y-coordinate of the rectangle's position.
+    Rectangle class that is inheriting from Base Class
+    Attr:
+    width: the rectangle's width
+    height: the rectangle's height
+    x: the rectangle's x coordinate
+    y: the rectangle's y coordinate
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Initialize a Rectangle object.
-        
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-            x (int, optional): x-coordinate of the rectangle's position. Defaults to 0.
-            y (int, optional): y-coordinate of the rectangle's position. Defaults to 0.
-            id (int, optional): The ID for the object. If None, a new ID is assigned.
+        Constructor for initializing the rectangle object
+        Arguments:
+            width: the rectangle's width
+            height: the rectangle's height
+            x: the rectangle's x coordinate
+            y: the rectangle's y coordinate
+            id: the object ID
         """
         super().__init__(id)
-        self.__width = 0
-        self.__height = 0
-        self.__x = 0
-        self.__y = 0
-        self.width = width
         self.height = height
-        self.x = x
+        self.width = width
         self.y = y
+        self.x = x
