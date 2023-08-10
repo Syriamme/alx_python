3b3b8d5b6d518c2c04432b6f67ae1aaa32c14757
Syriamme <Roridaniel01@gmail.com>
2023-08-10 19:39:17 +0300

    Base Class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,28 +1,26 @@
 """
-models.base
-This module contains the Base class.
+models.base- The module containing the Base class.
 """
+
 
 class Base:
     """
-    Base class for other classes.
-    
-    Attributes:
-        __nb_objects (int): Private class attribute to keep track of object count.
-        id (int): Public instance attribute representing the object's ID.
+    Base class
+    Attrs:
+    __nb_objects (int): Private class attribute tracking the object count.
+    id (int): Public instance attribute that is showing the objects's ID.
     """
+    
     __nb_objects = 0
     
     def __init__(self, id=None):
         """
-        Initialize a Base object.
-        
+        Constructor        
         Args:
-            id (int, optional): The ID for the object. If None, a new ID is assigned.
+            id: The object ID.
         """
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
+            Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
-            
