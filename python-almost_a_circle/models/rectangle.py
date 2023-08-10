28f3838eb649522d6b48613e727e14eecffa560c
Syriamme <Roridaniel01@gmail.com>
2023-08-10 21:21:34 +0300

    the class Square that inherits from Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,53 +1,41 @@
 #!/usr/bin/python3
 
 """
-Module that defines Rectangle, a subclass of Base
+Module defining the Rectangle derived class
 """
 from models.base import Base
 
 
 class Rectangle(Base):
     """
-    class Rectangle, that inherits from Base
+    Rectangle class inheriting from the base class
     """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes a class Rectangle
 
-        Args:
-            width (int): Width of the rectangle
-            height (int): Height of the rectangle
-            x (int): X axis
-            y (int): Y axis
-            id (int): ID of the rectangle
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor for the class
+
+        Atrr:
+            width: Rectangle width
+            height: Rectangle width
+            
+            y: Y axis
+            x: X axis
+            id: object ID
         """
         super().__init__(id)
-        self.width = width
         self.height = height
-        self.x = x
+        self.width = width
         self.y = y
-
-    @property
-    def width(self):
-        """ Get the width of the rectangle """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Set the width of the rectangle """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+        self.x = x
 
     @property
     def height(self):
-        """ Get the width of the rectangle """
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Set the width of the rectangle """
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -55,13 +43,27 @@ class Rectangle(Base):
         self.__height = value
 
     @property
+    def width(self):
+        """wdith getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
     def x(self):
-        """ Get the width of the rectangle """
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Set the width of the rectangle """
+        """ x setter """
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -70,12 +72,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """ Get the width of the rectangle """
+        """ y getter """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Set the width of the rectangle """
+        """y setter"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -83,33 +85,38 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """ Method to calculate the area of a rectangle """
+        """calulating the rectangle area"""
+
         return (self.width * self.height)
 
     def display(self):
-        """Prints to stdout the rectangle instance"""
+        """ prints in stdout the Rectangle instance with the character #"""
+
         rectangle = self.y * "\n"
         for i in range(self.height):
-            rectangle += (" " * self.x)
-            rectangle += ("#" * self.width) + "\n"
-
+            rectangle = rectangle + (" " * self.x)
+            rectangle = rectangle + ("#" * self.width) + "\n"
         print(rectangle, end='')
 
+
+
     def __str__(self):
-        """Modifies the inbuilt method init"""
+        """Modifying the inbuilt"""
+
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.__class__.__name__, self.id, self.__x, self.__y,
             self.__width, self.__height)
+    
 
     def update(self, *args, **kwargs):
-        """ Update method
+        """ Update the class Rectangle by improving the public method def display(self)
 
         Args:
-            1st Item (int): Item at Index 0 is assigned to the id
-            2nd Item (int): Index 1 -> To width
-            3rd Item (int): Index 2 -> To height
-            4th Item (int): Index 3 -> To x
-            5th Item (int): Index 4 -> To y
+        1st arg: should be id attribute
+        2nd arg: should be width attribute
+        3rd arg: should be height attribute
+        4th arg: should be x attribute
+        5th arg: should be y attribute
         """
         if args is not None and len(args) > 0:
             for key, value in enumerate(args):
@@ -127,21 +134,23 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
-                if key == "width":
-                    self.width = value
                 if key == "height":
                     self.height = value
-                if key == "x":
-                    self.x = value
+                if key == "width":
+                    self.width = value
                 if key == "y":
                     self.y = value
+                if key == "x":
+                    self.x = value
+
 
     def to_dictionary(self):
-        """Returns the dictionary representation of a Rectangle"""
+        """Returns the dictionary which is a representation of a Rectangle"""
         return {
             "id": self.id,
-            "width": self.width,
             "height": self.height,
-            "x": self.x,
+            "width": self.width,
             "y": self.y
+            "x": self.x,
         }
+    
