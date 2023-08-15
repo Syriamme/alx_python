022a3c256e12941dd6000c3d7586526047a33b69
Syriamme <Roridaniel01@gmail.com>
2023-08-15 15:20:18 +0300

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 """
-This module takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter in the variable named 'q'. It then prints the response
-based on the specified conditions.
+-The module will take a letter,
+send a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter in the variable named 'q'
+-The modeule will then print the response
+on the basis of the given conditions.
 """
 
 import requests
@@ -11,22 +13,30 @@ import sys
 
 def search_user(letter):
     """
-    Sends a POST request to the specified URL with the given letter as a parameter,
-    and prints the response based on the conditions described.
+    -The module will take a letter,
+    send a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter in the variable named 'q'
+    -The modeule will then print the response
+    on the basis of the given conditions.
 
     Args:
-        letter (str): The letter to be sent as a parameter.
+        letter (str): The letter
     """
     url = 'http://0.0.0.0:5000/search_user'
+
     data = {'q': letter}
+
     response = requests.post(url, data=data)
 
     try:
         json_response = response.json()
+
         if json_response:
             print("[{}] {}".format(json_response['id'], json_response['name']))
+
         else:
             print("No result")
+            
     except ValueError:
         print("Not a valid JSON")
 
