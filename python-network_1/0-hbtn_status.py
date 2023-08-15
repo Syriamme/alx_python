5227a2f9cdf6cd5423778a597ee759e345d0d92f
Syriamme <Roridaniel01@gmail.com>
2023-08-15 14:09:45 +0300

    fetching resources from url

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+import requests
+
+def fetch_and_display_status(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception if there's an HTTP error
+        data = response.json()
+        
+        for key, value in data.items():
+            print(f"- {key}: {value}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    fetch_and_display_status(url)
