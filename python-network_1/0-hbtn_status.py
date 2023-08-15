d52f1f81952fb9415e8ec2ff773b8b2fcf463508
Syriamme <Roridaniel01@gmail.com>
2023-08-15 14:12:21 +0300

    fetching resources from url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,23 @@
+#!/usr/bin/env python3
+"""
+my_module
+
+This module is used to fetch the content from the specified URL (https://alu-intranet.hbtn.io/status)
+and display the body of the response with proper tabulation.
+"""
+
 import requests
 
-def fetch_and_display_status(url):
+def fetch_url_content():
     """
-    Fetches the given URL and displays the response body in a formatted way.
-    
-    Args:
-        url (str): The URL to fetch the status from.
+    Fetches the content from the specified URL and prints the response body.
+
+    The function uses the requests package to make a GET request to the URL and then prints
+    the content of the response, preceded by a tabulation.
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if there's an HTTP error
-        data = response.json()
-        
-        for key, value in data.items():
-            print(f"- {key}: {value}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('\t- {}'.format(response.text))
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    fetch_and_display_status(url)
+    fetch_url_content()
