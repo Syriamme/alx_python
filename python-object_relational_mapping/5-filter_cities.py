90183cf01c8615842afc335aa3becc3ebdfd514b
Syriamme <Roridaniel01@gmail.com>
2023-08-24 21:43:35 +0300

    a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,44 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" List all cities from a state """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <mysql_username> <mysql_password> <db_name> <state_name>")
         sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
+    username = sys.argv[1]
+    password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
-    )
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", 
+                          port=3306, 
+                          user=username, 
+                          passwd=password, 
+                          db=db_name)
 
-    # Create a cursor
+    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Construct the query using a parameterized query
-    query = ("SELECT cities.id, cities.name "
-             "FROM cities JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s "
-             "ORDER BY cities.id ASC")
-
-    # Execute the query to retrieve city names of the specified state
+    # Query for all cities in the provided state, 
+    # using a placeholder to prevent SQL injection
+    query = """
+        SELECT cities.id, cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
     cursor.execute(query, (state_name,))
 
-    # Fetch the results
-    results = cursor.fetchall()
-
-    # Print the results
-    for row in results:
+    # Fetch all the results
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
     # Close the cursor and the connection
