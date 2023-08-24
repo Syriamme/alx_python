424b6c16caeb675e1e5eccbcd128d62be9aded65
Syriamme <Roridaniel01@gmail.com>
2023-08-24 08:52:52 +0300

    a script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,39 @@
-#!/usr/bin/python3
-
-'''
-Script that lists all State objects from the database hbtn_0e_6_usa
-
-The script should take 3 arguments: mysql username,
-    mysql password and database name
-'''
+#!/usr/bin/env python3
+"""
+Script to list all State objects from the database hbtn_0e_6_usa.
+"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+def list_states(username, password, db_name):
+    """List all State objects from the database."""
+    # Create a connection to the database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # Query the State objects and sort by states.id
+    states = session.query(State).order_by(State.id).all()
+    
+    # Display the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <db_name>")
+        sys.exit(1)
+    
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    
+    # Importing these here to ensure the script isn't executed on import
+    from model_state import Base, State
+    
+    list_states(mysql_username, mysql_password, db_name)
