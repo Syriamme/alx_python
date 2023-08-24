be08c2414d59e5755a81cb3e4e825c8b105af896
Syriamme <Roridaniel01@gmail.com>
2023-08-24 19:04:35 +0300

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-'''
-script that takes in an argument and displays all values in
-    the states table of hbtn_0e_0_usa where name
-    matches the argument
-
-Script should take 4 arguments: mysql username,
-    mysql password, database name and state name searched
-'''
+"""
+a script that takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    mysql_usname = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    dbt_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_usname,
+        passwd=mysql_pass,
+        db=dbt_name
+    )
+    cursor = db.cursor()
+
+    query = """
+    SELECT *
+    FROM states
+    WHERE name LIKE %s
+    ORDER BY id ASC"
+    """
+
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
