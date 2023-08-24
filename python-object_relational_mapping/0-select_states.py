41a90041ec5325b4140cac7a560dc0dd8f22988a
Syriamme <Roridaniel01@gmail.com>
2023-08-24 17:57:25 +0300

    a script that lists all states from a database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    mysql_usname = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    dbt_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
+        user=mysql_usname,
+        passwd=mysql_pass,
+        db=dbt_name
     )
     cursor = db.cursor()
 
