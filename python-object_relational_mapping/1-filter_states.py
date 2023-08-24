38314329452a581ba76eb34fff01d459962dbb2e
Syriamme <Roridaniel01@gmail.com>
2023-08-24 18:21:16 +0300

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
 
+"""
+a script that lists a states from the database htbn_0e_0_usa
+starting with N (upper N
+The script will take three arguments: username, password and name
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <db_name>")
         sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    mysql_usname = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    dbt_name = sys.argv[3]
 
-    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
+        user=mysql_usname,
+        passwd=mysql_pass,
+        db=dbt_name
     )
-
-    # Create a cursor
     cursor = db.cursor()
 
-    # Execute a query to retrieve states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch the results
     results = cursor.fetchall()
 
-    # Print the results
     for row in results:
         print(row)
 
-    # Close the cursor and the connection
     cursor.close()
     db.close()
