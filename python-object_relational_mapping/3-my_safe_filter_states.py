c48379b61baa40a6be1d1e0c4e95abb687f3abd6
Syriamme <Roridaniel01@gmail.com>
2023-08-24 21:08:57 +0300

    write one that is safe from MySQL injections!

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
 
-'''
+"""
 A script that prevents SQL injection
-'''
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states""")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    mysql_usnm = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    db_nm = sys.argv[3]
+    ste_nm = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_usnm,
+        passwd=mysql_pass,
+        db=db_nm
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    cursor.execute(query, (ste_nm,))
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
