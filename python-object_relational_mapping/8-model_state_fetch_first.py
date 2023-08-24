3d30750ee1f2321bbcb0f0fa3de002afcc363fd6
Syriamme <Roridaniel01@gmail.com>
2023-08-24 22:32:20 +0300

    a script that prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,30 +7,25 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./script.py mysql_username mysql_password database_name")
         sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_nm = sys.argv[3]
 
-    # Create an engine and connect to the MySQL server
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    connecting_string = (f'mysql+mysqldb://{user}:{passwd}'
+                         f'@localhost:3306/{db_nm}')
+    engine = create_engine(connecting_string)
     
-    # Create a Session class, bound to this engine
     Session = sessionmaker(bind=engine)
 
-    # Create a session instance
     session = Session()
-
-    # Query for the first state
+    
     state = session.query(State).order_by(State.id).first()
 
-    # Check if state exists, else print "Nothing"
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
-
-    # Close the session
-    session.close()
+        
+        session.close()
