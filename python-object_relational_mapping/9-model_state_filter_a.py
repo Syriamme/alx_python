588a579a40ef893d8faa49edf71635aab014f936
Syriamme <Roridaniel01@gmail.com>
2023-08-24 22:56:25 +0300

    a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 a script that lists all State objects
 that contain the letter a from the database hbtn_0e_6_usa
 """
+
 
 import sys
 from sqlalchemy import create_engine
