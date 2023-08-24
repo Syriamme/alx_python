f9a65c413416f224113de485804df91fc3e4df3a
Syriamme <Roridaniel01@gmail.com>
2023-08-24 22:21:30 +0300

    a python file that contains the class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-'''
-Script that contains the class definition of a State
+"""
+a python file that contains the class definition of a State
 and an instance Base = declarative_base()
-'''
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,9 +12,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    '''
-    Inherits from Base class
-    '''
+    """
+    Inheriting from Base class
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
