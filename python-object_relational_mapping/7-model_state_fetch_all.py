26f8e3b92663f0dc45cef4f91385ebe5e50699c2
Syriamme <Roridaniel01@gmail.com>
2023-08-24 08:57:49 +0300

    a script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,15 +11,17 @@ from model_state import Base, State
 def list_states(username, password, db_name):
     """List all State objects from the database."""
     # Create a connection to the database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
-    
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+    )
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     # Query the State objects and sort by states.id
     states = session.query(State).order_by(State.id).all()
-    
+
     # Display the results
     for state in states:
         print(f"{state.id}: {state.name}")
@@ -28,12 +30,12 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <mysql_username> <mysql_password> <db_name>")
         sys.exit(1)
-    
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
-    
+
     # Importing these here to ensure the script isn't executed on import
     from model_state import Base, State
-    
+
     list_states(mysql_username, mysql_password, db_name)
