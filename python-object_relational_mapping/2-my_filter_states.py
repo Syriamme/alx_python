286d8262f7ad803a11c7eaef2acb2d3417007e61
Syriamme <Roridaniel01@gmail.com>
2023-08-24 20:37:00 +0300

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,46 +1,44 @@
 #!/usr/bin/env python3
 
 """
-a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
+a script that takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa 
 where name matches the argument
 """
 
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    mysql_usname = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    db_nm = sys.argv[3]
+    state_nm = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
+        user=mysql_usname,
+        passwd=mysql_pass,
+        db=db_nm
     )
 
     cursor = db.cursor()
 
-    query = """
-    SELECT *
-    FROM states
-    WHERE BINARY name = %s
-    ORDER BY id ASC
-    """
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
 
-    cursor.execute(query, (state_name,))
+    cursor.execute(query, (state_nm,))
 
     results = cursor.fetchall()
 
     for row in results:
         print(row)
+
 
     cursor.close()
     db.close()
