a298cafd7652157fc60e681c15942b8e630f3d4c
Syriamme <Roridaniel01@gmail.com>
2023-08-31 13:23:15 +0300

    a script that starts a Flask web application

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,29 @@
+"""
+Script that starts a Flask web application
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable 
+(replace underscore _ symbols with a space )
+"""
+
+from flask import Flask
+from flask import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    text = escape(text.replace('_', ' '))
+    return f"C {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
