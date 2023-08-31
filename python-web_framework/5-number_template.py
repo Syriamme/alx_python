be1dc1a540889e7c550180af4b09512e19b306ef
Syriamme <Roridaniel01@gmail.com>
2023-08-31 14:33:25 +0300

    script

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,7 +1,15 @@
 """
 A script that starts a Flask web application
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY
 """
-
 
 from flask import Flask, render_template
 from flask import escape
