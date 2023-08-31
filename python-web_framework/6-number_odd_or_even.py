915e29ae9e28ff7fd677db21e66bd8ccece9054f
Syriamme <Roridaniel01@gmail.com>
2023-08-31 15:43:16 +0300

    script that starts a Flask web application

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -45,10 +45,10 @@ def display_number(n):
 def display_number_template(n):
     return render_template("5-number.html", n=n)
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def display_number_odd_or_even(n):
     num = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', number=n, num=num)
+    return render_template("6-number_odd_or_even.html", n=n, num=num)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
