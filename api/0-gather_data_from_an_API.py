7be5d3d8a9d0b8636c1adda46e26ac3fa4fc9d1c
Syriamme <roridaniel01@gmail.com>
2023-10-10 07:43:20 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 
-import json
-import urllib.error
 import urllib.request
+import json
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
-
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
         with urllib.request.urlopen(employee_url) as response:
-            employee_data = json.loads(response.read().decode())
+            if response.getcode() == 200:
+                employee_data = json.loads(response.read().decode())
+            else:
+                print(f"Error: Unable to fetch employee details. Status Code: {response.getcode()}")
+                return
         
         with urllib.request.urlopen(todo_url) as response:
-            todo_data = json.loads(response.read().decode())
+            if response.getcode() == 200:
+                todo_data = json.loads(response.read().decode())
+            else:
+                print(f"Error: Unable to fetch TODO list. Status Code: {response.getcode()}")
+                return
 
         total_tasks = len(todo_data)
-        
-        completed_tasks = 0
-        for task in todo_data:
-            if task['completed']:
-                completed_tasks += 1
-                print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
+        completed_tasks = sum(1 for task in todo_data if task['completed'])
+
+        print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
         
         for task in todo_data:
             if task['completed']:
