f99ce2c0a7f245f186a3285a249392973676cedb
Syriamme <roridaniel01@gmail.com>
2023-10-10 10:21:23 +0300

    CSV Format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import csv
+import json
+import sys
+import urllib.request
+
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+
+    try:
+        with urllib.request.urlopen(employee_url) as response:
+            if response.getcode() == 200:
+                employee_data = json.loads(response.read().decode())
+                employee_name = employee_data["name"]
+            else:
+                print(f"Error: Unable to fetch employee details. Status Code: {response.getcode()}")
+                return
+
+        with urllib.request.urlopen(todo_url) as response:
+            if response.getcode() == 200:
+                todo_data = json.loads(response.read().decode())
+            else:
+                print(f"Error: Unable to fetch TODO list. Status Code: {response.getcode()}")
+                return
+
+        total_tasks = len(todo_data)
+        completed_tasks = sum(1 for task in todo_data if task['completed'])
+
+        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+
+        with open(f"{employee_id}.csv", 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in todo_data:
+                csv_writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
+    
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Please enter a valid integer for the employee ID.")
