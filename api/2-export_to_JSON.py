c409f95c0ae4254d9d6f3ccdd65f9ba27ce0942f
Syriamme <roridaniel01@gmail.com>
2023-10-12 00:18:54 +0300

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -28,7 +28,6 @@ def export_employee_todo_data(employee_id):
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
-        # Fetch employee information
         with urllib.request.urlopen(employee_url) as response:
             if response.getcode() == 200:
                 employee_data = json.loads(response.read().decode())
@@ -37,7 +36,6 @@ def export_employee_todo_data(employee_id):
                 print(f"Error: Unable to fetch employee details. Status Code: {response.getcode()}")
                 return
 
-        # Fetch TODO list
         with urllib.request.urlopen(todo_url) as response:
             if response.getcode() == 200:
                 todo_data = json.loads(response.read().decode())
@@ -45,15 +43,13 @@ def export_employee_todo_data(employee_id):
                 print(f"Error: Unable to fetch TODO list. Status Code: {response.getcode()}")
                 return
 
-        # JSON data structure
         user_data = {
-           str(employee_id): [
+            str(employee_id): [
                 {"task": task["title"], "completed": task["completed"], "username": employee_name}
                 for task in todo_data
             ]
         }
 
-        # Exporting data to a JSON file
         with open(f"{employee_id}.json", 'w') as outfile:
             json.dump(user_data, outfile, indent=4)
 
