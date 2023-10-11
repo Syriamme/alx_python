6b9d39f809af3e9b2396f9df96e3680244f96f28
Syriamme <roridaniel01@gmail.com>
2023-10-12 02:04:14 +0300

    json format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -29,9 +29,10 @@ Dependencies:
     - urllib.request: For making HTTP requests to the external API.
 """
 
+
 import json
 import sys
-import urllib.request
+import requests
 
 def export_employee_todo_data(employee_id):
     # Define the base URL and URLs for fetching employee and TODO data
@@ -40,36 +41,44 @@ def export_employee_todo_data(employee_id):
     todo_url = f"{base_url}/users/{employee_id}/todos"
 
     try:
-        # Fetch employee data
-        with urllib.request.urlopen(employee_url) as response:
-            if response.getcode() == 200:
-                employee_data = json.loads(response.read().decode())
-                employee_name = employee_data.get("username")
-            else:
-                print(f"Error: Unable to fetch employee details. Status Code: {response.getcode()}")
-                return
+        # Fetch employee data using requests
+        employee_response = requests.get(employee_url)
+        if employee_response.status_code == 200:
+            employee_data = employee_response.json()
+            employee_name = employee_data.get("username")
+        else:
+            print(f"Error: Unable to fetch employee details. Status Code: {employee_response.status_code}")
+            return
 
-        # Fetch TODO list data
-        with urllib.request.urlopen(todo_url) as response:
-            if response.getcode() == 200:
-                todo_data = json.loads(response.read().decode())
-            else:
-                print(f"Error: Unable to fetch TODO list. Status Code: {response.getcode()}")
-                return
+        # Fetch TODO list data using requests
+        todo_response = requests.get(todo_url)
+        if todo_response.status_code == 200:
+            todo_data = todo_response.json()
+        else:
+            print(f"Error: Unable to fetch TODO list. Status Code: {todo_response.status_code}")
+            return
 
-        # Create a dictionary to store user data and tasks
-        user_data = {
-            str(employee_id): [
-                {"task": task.get("title"), "completed": task.get("completed"), "username": employee_name}
-                for task in todo_data
-            ]
-        }
+        # Create a list to store tasks
+        tasks = []
 
-        # Export user data to a JSON file with an indent of 4 spaces
-        with open(f"{employee_id}.json", 'w') as outfile:
-            json.dump(user_data, outfile)
+        for task in todo_data:
+            task_info = {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": employee_name
+            }
+            tasks.append(task_info)
 
-    except urllib.error.URLError as e:
+        # Export data to JSON file as a dictionary with USER_ID and the list of tasks
+        result_data = {str(employee_id): tasks}
+
+        with open(f"{employee_id}.json", "w") as json_file:
+            json.dump(result_data, json_file)
+
+        # Print a success message
+        print(f"Data exported to {employee_id}.json")
+
+    except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
