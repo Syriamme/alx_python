57da7c1b6b04c6550fc18a16267fedea299f1153
Syriamme <roridaniel01@gmail.com>
2023-10-12 00:30:10 +0300

    export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -29,11 +29,9 @@ Dependencies:
     - urllib.request: For making HTTP requests to the external API.
 """
 
-import json
-import sys
-import urllib.request
 
-def get_employee_todo_progress(employee_id):
+def export_employee_todo_data(employee_id):
+
     base_url = "https://jsonplaceholder.typicode.com"
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"
@@ -42,34 +40,29 @@ def get_employee_todo_progress(employee_id):
         with urllib.request.urlopen(employee_url) as response:
             if response.getcode() == 200:
                 employee_data = json.loads(response.read().decode())
-                employee_name = employee_data["username"]
+                employee_name = employee_data["name"]
             else:
                 print(f"Error: Unable to fetch employee details. Status Code: {response.getcode()}")
                 return
 
         with urllib.request.urlopen(todo_url) as response:
             if response.getcode() == 200:
-                todo_data = json.loads(response.read().decode())
+                todo_data = json.loads response.read().decode())
             else:
                 print(f"Error: Unable to fetch TODO list. Status Code: {response.getcode()}")
                 return
 
-        # Create a list to store tasks
-        tasks = []
+        user_data = {
+            str(employee_id): [
+                {"task": task["title"], "completed": task["completed"], "username": employee_name}
+                for task in todo_data
+            ]
+        }
 
-        for task in todo_data:
-            task_info = {
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": employee_name
-            }
-            tasks.append(task_info)
+        with open(f"{employee_id}.json", 'w') as outfile:
+            json.dump(user_data, outfile, indent=4)
 
-        # Export data to JSON file as a dictionary with USER_ID and the list of tasks
-        result_data = {str(employee_id): tasks}
-
-        with open(f"{employee_id}.json", "w") as json_file:
-            json.dump(result_data, json_file, indent=4)
+        print(f"Data exported to {employee_id}.json")
 
     except urllib.error.URLError as e:
         print(f"Error: {e}")
@@ -81,6 +74,6 @@ if __name__ == "__main__":
 
     try:
         employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+        export_employee_todo_data(employee_id)
     except ValueError:
         print("Please enter a valid integer for the employee ID.")
